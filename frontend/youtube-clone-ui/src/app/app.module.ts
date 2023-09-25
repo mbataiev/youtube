d8b3cfc0b253b8db1f66d692b6326dad21e3e02a
mbataiev <max.bataiev@gmail.com>
2023-09-25 14:36:07 -0400

    added form to add video metadata

--- frontend/youtube-clone-ui/src/app/app.module.ts
+++ frontend/youtube-clone-ui/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { UploadVideoComponent } from './upload-video/upload-video.component';
-import {FormsModule} from "@angular/forms";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
 import {NgxFileDropModule} from "ngx-file-drop";
 import {MatButtonModule} from "@angular/material/button";
@@ -13,6 +13,11 @@ import { HeaderComponent } from './header/header.component';
 import {MatToolbarModule} from "@angular/material/toolbar";
 import {MatIconModule} from "@angular/material/icon";
 import { SaveVideoDetailsComponent } from './save-video-details/save-video-details.component';
+import {FlexLayoutModule} from "@angular/flex-layout";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatOptionModule} from "@angular/material/core";
+import {MatSelectModule} from "@angular/material/select";
+import {MatInputModule} from "@angular/material/input";
 
 @NgModule({
   declarations: [
@@ -30,7 +35,13 @@ import { SaveVideoDetailsComponent } from './save-video-details/save-video-detai
     NgxFileDropModule,
     MatButtonModule,
     MatToolbarModule,
-    MatIconModule
+    MatIconModule,
+    FlexLayoutModule,
+    MatFormFieldModule,
+    MatOptionModule,
+    MatSelectModule,
+    MatInputModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/youtube-clone-ui/src/app/save-video-details/save-video-details.component.ts
+++ frontend/youtube-clone-ui/src/app/save-video-details/save-video-details.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-save-video-details',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./save-video-details.component.css']
 })
 export class SaveVideoDetailsComponent {
+  saveVideoDetailsForm: FormGroup;
+  title: FormControl = new FormControl('');
+  description: FormControl = new FormControl('');
+  videoStatus: FormControl = new FormControl('');
 
+  constructor() {
+    this.saveVideoDetailsForm = new FormGroup({
+      title: this.title,
+      description: this.description,
+      videoStatus: this.videoStatus,
+    })
+  }
+
+  ngOnInit(): void {
+
+  }
 }
