f7decb739dabcf4f1ffadf5cde35f6e79849361c
mbataiev <max.bataiev@gmail.com>
2023-09-25 12:53:05 -0400

    added dto

---
+++ backend/src/main/java/com/maksym/project/youtubeclone/dto/VideoDto.java
@@ -0,0 +1,21 @@
+package com.maksym.project.youtubeclone.dto;
+
+import com.maksym.project.youtubeclone.model.VideoStatus;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class VideoDto {
+    private String id;
+    private String title;
+    private String description;
+    private Set<String> tags;
+    private String videoUrl;
+    private VideoStatus videoStatus;
+    private String thumbnailUrl;
+}
