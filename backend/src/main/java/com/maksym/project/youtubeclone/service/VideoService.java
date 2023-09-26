1ca39db723dd1403d90b7cc77d826a1f2dfdcec3
mbataiev <max.bataiev@gmail.com>
2023-09-25 21:07:03 -0400

    added get video details logic

--- backend/src/main/java/com/maksym/project/youtubeclone/service/VideoService.java
+++ backend/src/main/java/com/maksym/project/youtubeclone/service/VideoService.java
@@ -49,6 +49,21 @@ public class VideoService {
         return thumbnailUrl;
     }
 
+    public VideoDto getVideoDetails(String videoId){
+        Video savedVideo = getVideoById(videoId);
+
+        VideoDto videoDto = new VideoDto();
+        videoDto.setVideoUrl(savedVideo.getVideoUrl());
+        videoDto.setThumbnailUrl(savedVideo.getThumbnailUrl());
+        videoDto.setId(savedVideo.getId());
+        videoDto.setTitle(savedVideo.getTitle());
+        videoDto.setDescription(savedVideo.getDescription());
+        videoDto.setTags(savedVideo.getTags());
+        videoDto.setVideoStatus(savedVideo.getVideoStatus());
+
+        return videoDto;
+    }
+
     private Video getVideoById(String videoId) {
         return videoRepository.findById(videoId).orElseThrow(
                 () -> new IllegalArgumentException(String.format("Cannot find video by id -> %s", videoId))
